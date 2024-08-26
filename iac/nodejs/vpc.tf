883466ff765aa7fe090ac8af047cc487180bc071
devraghavm <medapati.raghu@gmail.com>
2024-08-25 23:37:39 -0600

    Update security group configuration for RDS proxy and RDS instances

--- iac/nodejs/vpc.tf
+++ iac/nodejs/vpc.tf
@@ -48,6 +48,7 @@ resource "aws_security_group" "rds_proxy_sg" {
     from_port       = 1433
     to_port         = 1433
     protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
     security_groups = [aws_security_group.lambda_sg.id]
   }
   egress {
@@ -63,20 +64,13 @@ resource "aws_security_group" "rds_sg" {
   description = "Allow access to RDS Proxy"
   vpc_id      = data.aws_vpc.default.id
 
-  ingress = [
-    {
-      from_port   = 1433
-      to_port     = 1433
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    },
-    {
-      from_port       = 1433
-      to_port         = 1433
-      protocol        = "tcp"
-      security_groups = [aws_security_group.lambda_sg.id]
-    }
-  ]
+  ingress = {
+    from_port       = 1433
+    to_port         = 1433
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+    security_groups = [aws_security_group.lambda_sg.id]
+  }
   egress {
     from_port   = 0
     to_port     = 0
